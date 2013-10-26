14e3152b915259037147cd6f0e3fcfc9c5aa362e
dziub3k <dziub3k@gmail.com>
2013-10-26 14:52:30 +0200

    Wykres

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new Ob\HighchartsBundle\ObHighchartsBundle(),
             
             new RJ\PublicBundle\RJPublicBundle(),
             new RJ\UserBundle\RJUserBundle(),
