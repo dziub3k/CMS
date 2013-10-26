6037313dd59de57d1de93d0f207811627eda979f
dziub3k <dziub3k@gmail.com>
2013-10-26 23:01:44 +0200

    drobne poprawki styli

--- src/RJ/AdminBundle/Controller/DefaultController.php
+++ src/RJ/AdminBundle/Controller/DefaultController.php
@@ -14,16 +14,25 @@ class DefaultController extends Controller
             array("name" => "Data Serie Name",    "data" => array(1,2,4,5,6,3,8))
         );
 
-        $ob = new Highchart();
-        $ob->chart->renderTo('linechart');  // The #id of the div where to render the chart
-        $ob->title->text('Chart Title');
-        $ob->xAxis->title(array('text'  => "Horizontal axis title"));
-        $ob->yAxis->title(array('text'  => "Vertical axis title"));
-        $ob->series($series);
+        $ob1 = new Highchart();
+        $ob1->chart->renderTo('linechart1');  // The #id of the div where to render the chart
+        $ob1->title->text('Chart Title');
+        $ob1->xAxis->title(array('text'  => "Horizontal axis title"));
+        $ob1->yAxis->title(array('text'  => "Vertical axis title"));
+        $ob1->series($series);
+        $ob2 = new Highchart();
+        $ob2->chart->renderTo('linechart2');  // The #id of the div where to render the chart
+        $ob2->title->text('Statystyki sprzedaży');
+        $ob2->xAxis->title(array('text'  => "Horizontal axis title"));
+        $ob2->yAxis->title(array('text'  => "Vertical axis title"));
+        $ob2->series($series);
 
         return $this->render(
             'RJAdminBundle:Default:index.html.twig',
-            array('chart' => $ob)
+            array(
+                'chart1' => $ob1,
+                'chart2' => $ob2
+            )
         );
     }
 }
