510676d4feace90e9495e30e425cb519f61b5f34
dziub3k <dziub3k@gmail.com>
2013-12-09 21:24:37 +0100

    dodanie MenuController.php

--- src/RJ/AdminBundle/Controller/MenuController.php
+++ src/RJ/AdminBundle/Controller/MenuController.php
@@ -4,10 +4,10 @@ namespace RJ\AdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class SettingsController extends Controller {
+class MenuController extends Controller {
 
     public function indexAction()
     {
         return $this->render('RJAdminBundle:Settings:index.html.twig');
     }
-} 
+}
